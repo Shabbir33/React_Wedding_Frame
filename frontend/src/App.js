dca77ba46eac696891f3330fd97ca573134aa52f
Shabbir Poonawala <shabbirpoonawala03@gmail.com>
2023-09-22 13:48:01 +0530

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BackGround from './components/BackGround';
+
+function App() {
+  return (
+    <div className="App">
+      <BackGround/>      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/BackGround.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './BackGround.css'
+
+const BackGround = () => {
+    return (
+        <> 
+            <div className="context">
+                <h1>Pure Css Animated Background</h1>
+            </div>
+
+
+            <div className="area" >
+                <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default BackGround
